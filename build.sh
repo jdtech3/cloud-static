0793033939046d14acaf0ebb3b6993f057bb2bb5
Jackyminer <macdougall.jack@gmail.com>
2019-08-20 17:54:33 -0700

    Added cloud.jackm.xyz info page

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ rm -rf dist
 rm -rf bin
 mkdir -p bin
 
-sites=( wip error 502 404 )
+sites=( wip error 502 404 jackmxyz-cloud )
 for i in "${sites[@]}"; do
 	npx parcel build "$i/index.html"
 	mv "dist" "bin/$i"
-	mv "$i/favicon.ico" "bin/$i/"
+	cp "$i/favicon.ico" "bin/$i/"
 done
