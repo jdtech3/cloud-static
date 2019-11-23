3385337be7f859194367accf834a4ccbaa2f5bfa
Jack MacDougall <luawhat@gmail.com>
2019-11-23 15:57:25 -0800

    Fixed build issue

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf dist
 rm -rf bin
 mkdir -p bin
 
-sites=( wip error 502 404 jackmxyz-cloud )
+sites=( wip error 502 404 lua-cloud )
 for i in "${sites[@]}"; do
 	npx parcel build "$i/index.html"
 	mv "dist" "bin/$i"
